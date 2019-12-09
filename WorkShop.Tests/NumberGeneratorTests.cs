8823bb0faaca270e86ecd1b377cb1bad02a4c08c
Piotr FLIS <piotr.flis@soprasteria.com>
2019-12-09 22:11:40 +0100

    simple class with tests

---
+++ WorkShop.Tests/NumberGeneratorTests.cs
@@ -0,0 +1,68 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Workshop;
+
+namespace WorkShop.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ShouldHaveDefaultValue0()
+        {
+            NumberGenerator generator = new NumberGenerator();
+
+            int expectedDefaultValue = NumberGenerator.DefaultValue;
+            Assert.AreEqual(expectedDefaultValue, generator.Number);
+        }
+
+        [TestMethod]
+        public void ShouldUseDefaultStep()
+        {
+            //given
+            NumberGenerator generator = new NumberGenerator();
+
+            //when
+            int nextNumber = generator.GenerateNextNumber();
+
+            //then
+            int expectedNextValue = NumberGenerator.DefaultValue + NumberGenerator.DefaultStep;
+            Assert.AreEqual(expectedNextValue, nextNumber);
+        }
+
+        [TestMethod]
+        public void ShouldGetLastNumber()
+        {
+            //given
+            NumberGenerator generator = new NumberGenerator();
+            int expectedNextValue = generator.GenerateNextNumber();
+
+            //when
+            int lastValue = generator.Number;
+
+            //then
+            Assert.AreEqual(expectedNextValue, lastValue);
+        }
+
+        [TestMethod]
+        public void ShouldGetPreviousNumber()
+        {
+            //given
+            NumberGenerator generator = new NumberGenerator();
+            int expectedNextValue = generator.GenerateNextNumber();
+
+            //when
+            int lastValue = generator.Number;
+
+            //then
+            Assert.AreEqual(expectedNextValue, lastValue);
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void ShouldGetDefaultValueAsPreviousIfNothingGenerated()
+        {
+        }
+
+    }
+}
+
---
+++ Workshop/NumberGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Workshop
+{
+    public class NumberGenerator
+    {
+        public const int DefaultValue = 0;
+        public const int DefaultStep = 10;
+
+        public int Number { get; private set; }
+
+        public NumberGenerator()
+        {
+            Number = DefaultValue;
+            Step = DefaultStep;
+        }
+        
+        public int Step;
+
+        public int GenerateNextNumber()
+        {
+            Number += Step;
+            return Number;
+        }
+
+        public int PreviousNumber { get => Number - Step; }
+    }
+}
