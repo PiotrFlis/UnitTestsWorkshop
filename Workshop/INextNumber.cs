098e2e9f92269f86079717a0873d994b94243652
Piotr FLIS <piotr.flis@soprasteria.com>
2020-01-28 21:53:48 +0100

    Add INextNumber interface

---
+++ Workshop/INextNumber.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Workshop
+{
+    public interface INextNumber
+    {
+        int GetNextNumber(int previousNumber);
+    }
+}
